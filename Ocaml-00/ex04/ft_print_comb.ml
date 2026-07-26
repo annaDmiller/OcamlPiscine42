69bfdd65eea6c64fa523cb93b82d6541bedbd273
Anna Melnikova <amelniko@student.42.fr>
2026-07-26 14:25:30 +0200

    ex04 is done

---
+++ Ocaml-00/ex04/ft_print_comb.ml
@@ -0,0 +1,28 @@
+let rec print_num_comb (one : int) (two : int) (three : int) = 
+    if three <= 9 && two <= 9 && one <= 9 then (
+        if three > two && two > one && three <> two  && two <> one then (
+            print_int one;
+            print_int two;
+            print_int three;
+            if one <> 7 || two <> 8 || three <> 9 then 
+                print_string ", "
+        );
+        if three <> 9 then
+            print_num_comb one two (three + 1)
+        else (
+            if two <> 9 then
+                print_num_comb one (two + 1) 0
+            else (
+                print_num_comb (one + 1) 0 0
+            )
+        )
+    ) else
+        print_string "\n"
+  
+let ft_print_comb () = 
+    let first = 0 in
+    let second = 1 in
+    let third = 2 in
+    print_num_comb first second third
+
+(*let () = ft_print_comb()*)
