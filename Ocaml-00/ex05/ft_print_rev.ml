b91973944e85319a5e8b11c094ec99dab2056b11
Anna Melnikova <amelniko@student.42.fr>
2026-07-26 18:27:56 +0200

    ex05 is done

---
+++ Ocaml-00/ex05/ft_print_rev.ml
@@ -0,0 +1,18 @@
+let rec print_rev string ind = 
+    if ind >= 0 then (
+        print_char (String.get string ind);
+        print_rev string (ind - 1)
+    )
+
+
+let ft_print_rev string =
+    print_rev string (String.length string - 1);
+    print_char '\n'
+
+
+(*let () = 
+    ft_print_rev "Hello World !";
+
+    print_endline "-------";
+
+    ft_print_rev ""*)
