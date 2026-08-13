c7a84bd6586d7b1fd2cdb2bda2140ab5be4f973c
Anna Melnikova <amelniko@student.42.fr>
2026-08-13 21:28:05 +0200

    ex01: adding unit tests

--- Ocaml-00-02/ex01/crossover.ml
+++ Ocaml-00-02/ex01/crossover.ml
@@ -14,3 +14,47 @@ let rec crossover lst_a lst_b =
             | val_a :: rest_a when is_present val_a lst_b -> val_a :: crossover rest_a lst_b
             | _ :: rest_a -> crossover rest_a lst_b
     )
+
+
+
+let rec print_lst_int arg =
+    match arg with
+        | [] -> print_endline "]"
+        | [value] -> (
+            print_int value;
+            print_endline "]"
+        )
+        | value :: rest -> (
+            print_int value;
+            print_string "; ";
+            print_lst_int rest
+        )
+
+let () =
+    print_endline "crossover [0; 1; 2; 3; 4; 5; 6; 7; 8] and [6; 7; 8; 9; 10]";
+    print_char '[';
+    let first_lst = [0; 1; 2; 3; 4; 5; 6; 7; 8]
+    and second_lst = [6; 7; 8; 9; 10]
+    in
+    print_lst_int @@ crossover first_lst second_lst;
+
+    print_endline "crossover [] and [6; 7; 8; 9; 10]";
+    print_char '[';
+    let first_lst = []
+    and second_lst = [6; 7; 8; 9; 10]
+    in
+    print_lst_int @@ crossover first_lst second_lst;
+
+    print_endline "crossover [0; 1; 2; 3; 4; 5; 6; 7; 8] and []";
+    print_char '[';
+    let first_lst = [0; 1; 2; 3; 4; 5; 6; 7; 8]
+    and second_lst = []
+    in
+    print_lst_int @@ crossover first_lst second_lst;
+
+    print_endline "crossover [0; 1; 2; 3; 4; 5] and [6; 7; 8; 9; 10]";
+    print_char '[';
+    let first_lst = [0; 1; 2; 3; 4; 5]
+    and second_lst = [6; 7; 8; 9; 10]
+    in
+    print_lst_int @@ crossover first_lst second_lst
