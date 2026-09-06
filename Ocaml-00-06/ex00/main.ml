904f86ed0f25bea4f83607d7ece844c5341a0c7b
Anna Melnikova <amelniko@student.42.fr>
2026-09-06 14:34:28 +0200

    ex00 is done

---
+++ Ocaml-00-06/ex00/main.ml
@@ -0,0 +1,9 @@
+let () =
+    print_endline "==== Testing the people class ====";
+    let clara = new People.people "Clara" in
+    print_string "to_sting signal:  ";
+    print_endline clara#to_string;
+    print_string "talk signal:  ";
+    clara#talk;
+    print_string "die signal:  ";
+    clara#die;
